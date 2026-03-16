63a18226221ac556b30f8e67e82d3bade7050d81
AMRIT PAL SINGH <126061621+amt-12@users.noreply.github.com>
2026-03-16 10:20:47 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import {
   UserOutlined,
   VideoCameraOutlined,
 } from "@ant-design/icons";
-import { Button, Layout, Menu, theme } from "antd";
+import {  Layout, Menu, theme } from "antd";
 import ContentOne from "./Components/ContentOne";
 import ContentTwo from "./Components/ContentTwo";
 import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 
+
+import { Button, message, Upload } from 'antd';
 const { Header, Sider, Content } = Layout;
 const App = () => {
   const [collapsed, setCollapsed] = useState(false);
   const [key, setKey] = useState("1");
-
+  const [imageData, setImageData] = useState(null);
+console.log("imageData", imageData);
 // useEffect(() => {
   const userToken = localStorage.getItem("jwtToken");
   const userRole = jwtDecode(userToken)
@@ -26,6 +30,40 @@ const App = () => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  const props = {
+    name: 'file',
+    action: 'http://localhost:5002/api/adhaarTeam/uploadImage',
+    headers: {
+      contentType: "multipart/form-data",
+      // authorization: "multiple/form-data",
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log("image data",info.file.originFileObj);
+        setImageData(info.file.originFileObj);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+const handleApi =()=>{
+try {
+  const uploadData = new FormData();
+  uploadData.append("file", imageData);
+console.log("uploadData", uploadData);
+  axios.post("http://localhost:5002/api/adhaarTeam/uploadImage", uploadData)
+  .then((response) => {
+    console.log("response from api", response);
+  })
+  
+} catch (error) {
+  console.log("error while fetching data from api", error);
+}
+}
+
 
 const getMenuItems = () => {
   const menuItems = [
@@ -104,6 +142,10 @@ console.log("menu items based on user role", items);
             borderRadius: borderRadiusLG,
           }}
         >
+        <Upload {...props}>
+    <Button icon={<UploadOutlined />}>Click to Upload</Button>
+  </Upload>
+  <Button onClick={handleApi}>Make API Call</Button>
           {key === "1" ? (
             <ContentOne />
           ) : key === "2" ? (
